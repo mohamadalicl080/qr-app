0789165e8f6521015452f6b44edf1a590b40a5a2
mohamadalicl080 <mohamadalicl080@gmail.com>
2025-07-16 18:46:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ async function checkVehicleStatus(plate) {
                 showStatus('green', `Autorizado: ${match[1] || 'Sin nombre'} (${normalizedPlate})`);
             }
         } else {
-            showStatus('purple', 'Patente no encontrada en el registro');
+            showStatus('green', 'Patente no encontrada en el registro');
         }
 
     } catch (error) {
